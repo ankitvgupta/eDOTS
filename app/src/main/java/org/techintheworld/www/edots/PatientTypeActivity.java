4c79bb748709fbf817ac8ac57b33f82cc2a2136e
Nishant Kakar <nkakar@college.harvard.edu>
2015-01-06 15:16:17 -0500

    fixed Logout button

--- app/src/main/java/org/techintheworld/www/edots/PatientTypeActivity.java
+++ app/src/main/java/org/techintheworld/www/edots/PatientTypeActivity.java
@@ -48,4 +48,9 @@ public class PatientTypeActivity extends Activity {
         Intent intent = new Intent(this, GetPatientActivity.class);
         startActivity(intent);
     }
+
+    public void switchLogin(View view) {
+        Intent intent = new Intent(this, WorkerLoginActivity.class);
+        startActivity(intent);
+    }
 }
