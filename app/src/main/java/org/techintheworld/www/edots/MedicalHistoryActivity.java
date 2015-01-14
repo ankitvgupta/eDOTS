76a7b5e3d00bde5eaa1831b332f89c639c7b1a65
Nishant Kakar <nkakar@college.harvard.edu>
2015-01-14 16:35:46 -0500

    attempting to make progress on calendar

--- app/src/main/java/org/techintheworld/www/edots/MedicalHistoryActivity.java
+++ app/src/main/java/org/techintheworld/www/edots/MedicalHistoryActivity.java
@@ -2,8 +2,13 @@ package org.techintheworld.www.edots;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
@@ -38,11 +43,51 @@ public class MedicalHistoryActivity extends Activity {
             e.printStackTrace();
         }
 
-//        TextView test = (TextView) findViewById(R.id.medicalhistory1);
-//        test.setText(currentPatient.toString());
-//
-//        Log.v("MedicalHistoryActivity.java", "The ArrayList of visits are: " + currentPatient.getPatientHistory());
+        Context ctx = getApplicationContext();
+        createCalendar(ctx);
 
+//        loadPastVisits();
+    }
+
+    // creates the values the Calendar will have
+    private static ContentValues buildNewCalContentValues() {
+        final ContentValues cv = new ContentValues();
+        cv.put(CalendarContract.Calendars.ACCOUNT_NAME, "Nish");
+        cv.put(CalendarContract.Calendars.ACCOUNT_TYPE, CalendarContract.ACCOUNT_TYPE_LOCAL);
+        cv.put(CalendarContract.Calendars.NAME, "Nish's Calendar");
+        cv.put(CalendarContract.Calendars.CALENDAR_DISPLAY_NAME, "Nish's Calendar");
+        cv.put(CalendarContract.Calendars.CALENDAR_COLOR, 0xEA8561);
+        cv.put(CalendarContract.Calendars.CALENDAR_ACCESS_LEVEL, CalendarContract.Calendars.CAL_ACCESS_READ);
+        cv.put(CalendarContract.Calendars.OWNER_ACCOUNT, "Nish");
+        cv.put(CalendarContract.Calendars.VISIBLE, 1);
+        cv.put(CalendarContract.Calendars.SYNC_EVENTS, 1);
+        return cv;
+    }
+
+    /**The main/basic URI for the android calendars table*/
+    private static final Uri Cal_URI = CalendarContract.Calendars.CONTENT_URI;
+
+    /**Builds the Uri for your Calendar in android database (as a Sync Adapter)*/
+    private static Uri buildCalUri() {
+        return Cal_URI
+                .buildUpon()
+                .appendQueryParameter(CalendarContract.CALLER_IS_SYNCADAPTER, "true")
+                .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_NAME, "Nish")
+                .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_TYPE,
+                        CalendarContract.ACCOUNT_TYPE_LOCAL)
+                .build();
+    }
+
+    // Create and insert new android calendar into database
+    public static void createCalendar(Context ctx) {
+        ContentResolver cr = ctx.getContentResolver();
+        final ContentValues cv = buildNewCalContentValues();
+        Uri calUri = buildCalUri();
+        //insert the calendar into the database
+        cr.insert(calUri, cv);
+    }
+
+    public void loadPastVisits() {
         // sets header to Past Visits for Patient Name
         String patientName = currentPatient.getName();
         TextView header = (TextView) findViewById(R.id.medical_history_for_patient);
@@ -142,7 +187,6 @@ public class MedicalHistoryActivity extends Activity {
             v.setBackgroundColor(Color.parseColor("#B3B3B3"));
             encloseScrollLayout.addView(v);
         }
-
     }
 
 
