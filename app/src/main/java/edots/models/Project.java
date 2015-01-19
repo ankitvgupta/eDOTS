e2164df7d43400b7f81afffc8ea31868b536435c
Brendan Bozorgmir <bbozorgmir@college.harvard.edu>
2015-01-19 14:25:26 -0500

    refactored projects

--- app/src/main/java/edots/models/Project.java
+++ app/src/main/java/edots/models/Project.java
@@ -18,8 +18,6 @@ import java.util.Random;
 public class Project {
     // TODO: consistent capitalization of all class variables
     private String Id;
-    private ArrayList<String> medications = new ArrayList<String>();
-    private ArrayList<String> types = new ArrayList<String>();
     private String name;
 
 
@@ -30,9 +28,6 @@ public class Project {
             int num = r.nextInt(100);
             Id = "5";
             name = Integer.toString(num);
-            medications = new ArrayList<String>();
-            medications.add("Med 1");
-            medications.add("Med 2");
         }
         catch (Exception e){
             e.printStackTrace();
@@ -48,11 +43,6 @@ public class Project {
             JSONObject n = new JSONObject(JSONString);
             Id = n.get("projectId").toString();
             name = n.get("name").toString();
-            JSONArray temp = new JSONArray(n.get("medications").toString());
-            medications = new ArrayList<String>();
-            for (int i = 0; i < temp.length(); i++){
-                medications.add(temp.get(i).toString());
-            }
         }
         catch (JSONException e) {
             e.printStackTrace();
@@ -77,10 +67,6 @@ public class Project {
         JSONObject temp = new JSONObject();
         try {
             temp.put("projectId", getId());
-            JSONArray version = new JSONArray(getMedications());
-            temp.put("medications", version);
-            JSONArray types = new JSONArray(getTypes());
-            temp.put("types", types);
             temp.put("name", getName());
         } catch (JSONException e) {
             Log.v("JSON Exception", "Found a JSON Exception");
@@ -97,19 +83,9 @@ public class Project {
         Id = id;
     }
 
-    public void setMedications(ArrayList<String> m){
-        medications = m;
-    }
-
     public void setName(String s){
        name = s;
    }
-
-    public ArrayList<String> getTypes(){return types;}
-    public ArrayList<String> getMedications(){
-        return medications;
-    }
-
 
     public String getName(){
         return name;
