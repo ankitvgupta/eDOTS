448d677c4704137f12d994e763e6d138c4b77cf3
ANKIT GUPTA <ankitgupta@college.harvard.edu>
2015-01-20 14:32:48 -0500

    fixed some stuff

--- app/src/main/java/org/techintheworld/www/edots/ChangeSchemaActivity.java
+++ app/src/main/java/org/techintheworld/www/edots/ChangeSchemaActivity.java
@@ -25,6 +25,8 @@ public class ChangeSchemaActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_schema);
+        loadTreatmentDayCheckboxes();
+
 
         try {
             currentPatient = new Patient(getIntent().getExtras().getString("Patient"));
